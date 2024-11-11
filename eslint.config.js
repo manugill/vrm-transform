02060889318fc52bfe26f39046112834fe0c20ab
Noeri Huisman <8823461+mrxz@users.noreply.github.com>
2024-11-11 21:51:04 +0100

    Use @stylistic/esling-plugin instead of TypeScript (-ts) version

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import stylisticTs from '@stylistic/eslint-plugin-ts';
+import stylistic from '@stylistic/eslint-plugin';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -11,25 +11,27 @@ export default [
   ...tseslint.configs.recommended,
   {
     plugins: {
-      '@stylistic/ts': stylisticTs
+      '@stylistic': stylistic
     },
     rules: {
-      '@stylistic/ts/indent': ['error', 2],
-      '@stylistic/ts/function-call-spacing': ['error', 'never'],
-      '@stylistic/ts/key-spacing': ['error'],
-      '@stylistic/ts/keyword-spacing': ['error', { 'overrides': { 'if': { 'after': false }, 'for': { 'after': false }, 'while': { 'after': false } } }],
-      '@stylistic/ts/lines-between-class-members': ['error', {
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/function-call-spacing': ['error', 'never'],
+      '@stylistic/key-spacing': ['error'],
+      '@stylistic/keyword-spacing': ['error', { 'overrides': { 'if': { 'after': false }, 'for': { 'after': false }, 'while': { 'after': false } } }],
+      '@stylistic/lines-between-class-members': ['error', {
         'enforce': [
           { 'blankLine': 'always', 'prev': 'method', 'next': 'method' }
         ]
       }],
-      '@stylistic/ts/member-delimiter-style': ['error', {
+      '@stylistic/member-delimiter-style': ['error', {
         'singleline': {
           'delimiter': 'comma',
           'requireLast': false
         },
       }],
-      '@stylistic/ts/quotes': ['error', 'single'],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/no-trailing-spaces': ['error'],
+      '@stylistic/no-multi-spaces': ['error'],
     }
   }
 ];
